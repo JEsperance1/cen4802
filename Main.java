2521c4b0a3ca680b34e1bfb860228fec597637f9
Jordan Esperance <115673342+JEsperance1@users.noreply.github.com>
2025-05-11 19:57:45 -0400

    Modified Main - Includes Javadoc Comments

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-package org.example.fibonacci;
-
-
-
 public class Main {
     public static void main(String[] args) {
 
@@ -10,6 +6,12 @@ public class Main {
     }
 
     public static int fib(int n) {
+        /**
+         *This is a recursive function that finds the value in the nth position of the fibonacci sequence.
+         *It does so by attempting to find the sum of (n-1) + (n-2) assuming n is not 1 or zero. The function
+         * calls itself over and over until it reaches the bas case and answers itself all the way back up. It returns
+         * the solution to the original function call and prints it as specified above.
+         */
         if (n == 0) {
             return 0;
         } else if (n == 1) {
